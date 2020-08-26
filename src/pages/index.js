a4f2b8fa6ad5d1db6d72a3d18a955bd0f9bf0890
bigcatplichta <bigcatplichta@gmail.com>
2020-08-26 16:21:48 -0600

    edit hero image properties

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ import React from "react";
 
 import Layout from "../components/layout";
 import SEO from "../components/seo";
-import wolfPackPicture from "../images/wolf1-4x3.png";
+import wolfPackPicture from "../images/two-wolves.png";
 
 function IndexPage() {
   return (
@@ -11,14 +11,14 @@ function IndexPage() {
         keywords={[`wolf`, `reintroduction`, `Colorado`, `prop`, `114`]}
         title="Home"
       />
-
-      <section className="text-center">
-        <img
+      
+      <img
           alt="Wolves running through wilderness."
-          className="bg-cover"
+          className="bg-cover bg-center"
           src={wolfPackPicture}
-        />
-
+      />
+      
+      <section className="text-center">
         <h2 className="inline-block p-3 mb-4 text-2xl font-bold bg-yellow-400">
           Hey there! Welcome to your first Gatsby site.
         </h2>
