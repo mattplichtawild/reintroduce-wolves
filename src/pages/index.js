21ae11ab876e82b5912b78d763b386b696e6a8f2
bigcatplichta <bigcatplichta@gmail.com>
2020-09-01 21:34:26 -0600

    add quote section

--- src/pages/index.js
+++ src/pages/index.js
@@ -69,9 +69,9 @@ function IndexPage() {
         </div>
       </section>
 
-      <section >
+      <section className="py-6">
         <div className="container flex flex-wrap mx-auto">
-          <h2 className="w-full inline-block mb-4 text-3xl text-center font-bold">The Facts of Wolf Reintroduction</h2>
+          <h2 className="w-full inline-block mb-4 text-3xl text-center font-bold">The Facts On Wolves</h2>
           <div className="w-1/3 flex flex-grow flex-shrink p-6">
             <div className="flex-1 bg-white rounded-t rounded-b shadow p-6">
               <h4 className="font-semibold pb-1 text-lg">First Fact</h4>
@@ -82,7 +82,7 @@ function IndexPage() {
           <div className="w-1/3 flex flex-grow flex-shrink p-6">
             <div className="flex-1 bg-white rounded-t rounded-b shadow p-6">
               <h4 className="font-semibold pb-1 text-lg">Second Fact</h4>
-              <p className="font-light">consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+              <p className="font-light">Consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
             </div>
           </div>
 
@@ -103,26 +103,29 @@ function IndexPage() {
           <div className="w-1/3 flex flex-grow flex-shrink p-6">
             <div className="flex-1 bg-white rounded-t rounded-b shadow p-6">
               <h4 className="font-semibold pb-1 text-lg">Fifth Fact</h4>
-              <p className="font-light">consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+              <p className="font-light"> Lorem ipsum dolor sit amet, consectetur adipiscing elit consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
             </div>
           </div>
 
           <div className="w-1/3 flex flex-grow flex-shrink p-6">
             <div className="flex-1 bg-white rounded-t rounded-b shadow p-6">
               <h4 className="font-semibold pb-1 text-lg">Sixth Fact</h4>
-              <p className="font-light">Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
+              <p className="font-light">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
             </div>
           </div>
         </div>
       </section>
 
-      <section className="text-center">
+      <section className="py-6">
         {/* Section with quotes from experts who support reintroduction */}
-
+        <h2 className="w-full inline-block mb-4 py-3 text-3xl text-center font-bold">Experts Support Reintroduction</h2>
+        <p className="leading-loose tracking-widest text-xl pr-32">&quot;The Yellowstone wolf re-establishment project makes clear that we can humanely and cost-effectively translocate wolves from one area to another and successfully reintroduce them. It makes clear, as does decades of reliable research from around the world, that wolves are not a threat to human safety or the livestock and big game hunting industries. It makes clear that co-existing with wolves is a very straightforward affair that requires only a modicum of accommodation on our part.&quot;</p>
+        <p>Mike Phillips - Biologist, Executive Director of Turner Endangered Species Fund, Montana State Senator</p>
       </section>
 
-      <section className="text-center">
-        {/* List of organizations that support reintroduction? */}
+      <section className="text-center py-6">
+        <h2 className="w-full inline-block mb-4 py-3 text-3xl text-center font-bold">The Opposition</h2>
+        {/* attack the opposing organization here */}
       </section>
       </div>
     </Layout>
