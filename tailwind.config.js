352dcb4282784e0eca7456c92dcbd87642f5244f
bigcatplichta <bigcatplichta@gmail.com>
2020-09-02 19:32:54 -0600

    update config for deprecated utilities

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,10 @@
 // See https://tailwindcss.com/docs/configuration for details
 module.exports = {
+    
+    future: {
+      removeDeprecatedGapUtilities: true,
+    },
   purge: ["./src/**/*.js"],
-  theme: {},
   variants: {},
   // https://github.com/tailwindcss/custom-forms
   plugins: [require("@tailwindcss/custom-forms")],
