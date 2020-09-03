6c97147755dbb2c4c5df036f134307cf9468a3dd
bigcatplichta <bigcatplichta@gmail.com>
2020-09-02 20:57:50 -0600

    update background color

--- src/components/header.js
+++ src/components/header.js
@@ -16,11 +16,11 @@ function Header() {
   `);
 
   return (
-    <header className="bg-teal-700">
-      <div className="flex flex-wrap items-center justify-between max-w-6xl p-4 mx-auto">
+    <header className="bg-primary">
+      <div className="flex flex-wrap items-center justify-between max-w-6xl p-8 mx-auto">
         <Link to="/">
           <h1 className="flex items-center text-white no-underline">
-            <svg
+            {/* <svg
               className="w-8 h-8 mr-2 fill-current"
               height="54"
               viewBox="0 0 54 54"
@@ -28,8 +28,8 @@ function Header() {
               xmlns="http://www.w3.org/2000/svg"
             >
               <path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z" />
-            </svg>
-            <span className="text-xl font-bold tracking-tight">
+            </svg> */}
+            <span className="uppercase text-xl font-bold tracking-tight">
               {site.siteMetadata.title}
             </span>
           </h1>
