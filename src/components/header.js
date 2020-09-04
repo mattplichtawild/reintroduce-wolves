94c6ecf60a38c4fb5f35023ef89d4b7e6beabc27
bigcatplichta <bigcatplichta@gmail.com>
2020-09-04 11:32:54 -0600

    add logo

--- src/components/header.js
+++ src/components/header.js
@@ -1,5 +1,6 @@
 import { graphql, useStaticQuery, Link } from "gatsby";
 import React, { useState } from "react";
+// import RestoreWolvesLogo from "../images/RestoreWolvesLogo.png";
 
 // Where is this all being pulled from? 
 
@@ -19,16 +20,8 @@ function Header() {
     <header className="bg-primary">
       <div className="flex flex-wrap items-center justify-between max-w-6xl p-8 mx-auto">
         <Link to="/">
+          {/* <img className="object-scale-down" src={RestoreWolvesLogo} alt="logo" /> */}
           <h1 className="flex items-center text-white no-underline">
-            {/* <svg
-              className="w-8 h-8 mr-2 fill-current"
-              height="54"
-              viewBox="0 0 54 54"
-              width="54"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z" />
-            </svg> */}
             <span className="uppercase text-xl font-bold tracking-tight">
               {site.siteMetadata.title}
             </span>
