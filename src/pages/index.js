7f549d8916e018d79868e6f54f9ef5748dce22f8
bigcatplichta <bigcatplichta@gmail.com>
2020-09-05 10:49:24 -0600

    add responsive classes

--- src/pages/index.js
+++ src/pages/index.js
@@ -19,20 +19,20 @@ function IndexPage() {
           className="bg-cover bg-center"
           src={wolfPackPicture}
       />
-      <div className="md:px-20 lg:px-32 bg-tertiary">
+      <div className="px-6 md:px-20 lg:px-32 bg-tertiary">
         <section className="text-center py-6">
-          <p className="leading-loose md:text-lg lg:text-xl font-sans mb-4">
+          <p className="text-sm md:leading-loose md:text-lg lg:text-xl font-sans mb-4">
             As Coloradans, we pride ourselves on the natural beauty of the state. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
           </p>
         </section>
       </div>
-      <div className="md:px-20 lg:px-32">
+      <div className="px-6 md:px-20 lg:px-32">
       <section id="about" className="text-center py-6">
         <h2 className="inline-block py-3 mb-4 text-3xl font-bold">
           About Proposition 114
         </h2>
 
-        <div className="grid gap-5 grid-cols-2 flex-auto text-2xl">
+        <div className="grid gap-5 grid-cols-2 flex-auto md:text-2xl">
           <ul className="list grid gap-8">
             <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</li>
             <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</li>
@@ -51,7 +51,7 @@ function IndexPage() {
       <div className="bg-white">
       <section id="proven-success" className="text-center py-6">
         <h2 className="inline-block py-3 mb-4 text-3xl font-bold">Proven Success in Yellowstone</h2>
-        <p className="text-lg md:px-20 lg:px-32">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Eu augue ut lectus arcu bibendum at varius vel pharetra. Felis imperdiet proin fermentum leo. Purus gravida quis blandit turpis cursus in hac habitasse. Nec sagittis aliquam malesuada bibendum. Ullamcorper dignissim cras tincidunt lobortis feugiat vivamus at augue eget. Ut sem viverra aliquet eget sit amet tellus. Lobortis scelerisque fermentum dui faucibus in. Morbi tincidunt ornare massa eget egestas purus.</p>
+        <p className="md:text-lg px-6 md:px-20 lg:px-32">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Eu augue ut lectus arcu bibendum at varius vel pharetra. Felis imperdiet proin fermentum leo. Purus gravida quis blandit turpis cursus in hac habitasse. Nec sagittis aliquam malesuada bibendum. Ullamcorper dignissim cras tincidunt lobortis feugiat vivamus at augue eget. Ut sem viverra aliquet eget sit amet tellus. Lobortis scelerisque fermentum dui faucibus in. Morbi tincidunt ornare massa eget egestas purus.</p>
 
         <div className="flex flex-wrap text-xl text-justify py-8">
           <div className="w-1/2 px-8">
@@ -122,21 +122,21 @@ function IndexPage() {
       <div className="xl:px-32 bg-blue-600">
       <section id="experts" className="py-6 text-gray-100">
         <h2 className="w-full inline-block mb-4 py-3 text-3xl text-center font-bold">Experts Support Reintroduction</h2>
-        <div className="xl:px-32 text-justify pb-12">
-          <p className="leading-loose tracking-widest text-xl">&quot;The Yellowstone wolf re-establishment project makes clear that we can humanely and cost-effectively translocate wolves from one area to another and successfully reintroduce them. It makes clear, as does decades of reliable research from around the world, that wolves are not a threat to human safety or the livestock and big game hunting industries. It makes clear that co-existing with wolves is a very straightforward affair that requires only a modicum of accommodation on our part.&quot;</p>
+        <div className="px-6 xl:px-32 text-justify pb-12">
+          <p className="leading-loose md:tracking-widest md:text-xl">&quot;The Yellowstone wolf re-establishment project makes clear that we can humanely and cost-effectively translocate wolves from one area to another and successfully reintroduce them. It makes clear, as does decades of reliable research from around the world, that wolves are not a threat to human safety or the livestock and big game hunting industries. It makes clear that co-existing with wolves is a very straightforward affair that requires only a modicum of accommodation on our part.&quot;</p>
           <p className="text-right">Mike Phillips - Biologist, Executive Director of Turner Endangered Species Fund, Montana State Senator</p>
         </div>
-        <div className="px-32 text-justify pb-8">
-          <p className="leading-loose tracking-widest text-xl">&quot;Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Etiam dignissim diam quis enim lobortis. Sed risus pretium quam vulputate dignissim suspendisse in est ante. Fermentum leo vel orci porta non. Purus sit amet volutpat consequat mauris nunc congue nisi. Egestas tellus rutrum tellus pellentesque eu tincidunt. Purus in mollis nunc sed id semper risus in. Eget velit aliquet sagittis id consectetur purus ut. Egestas maecenas pharetra convallis posuere morbi leo. Sollicitudin nibh sit amet commodo nulla facilisi nullam vehicula ipsum. Scelerisque eleifend donec pretium vulputate sapien nec sagittis aliquam. Nunc mattis enim ut tellus elementum sagittis vitae et leo.&quot;</p>
+        <div className="px-6 xl:px-32 text-justify pb-8">
+          <p className="leading-loose md:tracking-widest md:text-xl">&quot;Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Etiam dignissim diam quis enim lobortis. Sed risus pretium quam vulputate dignissim suspendisse in est ante. Fermentum leo vel orci porta non. Purus sit amet volutpat consequat mauris nunc congue nisi. Egestas tellus rutrum tellus pellentesque eu tincidunt. Purus in mollis nunc sed id semper risus in. Eget velit aliquet sagittis id consectetur purus ut. Egestas maecenas pharetra convallis posuere morbi leo. Sollicitudin nibh sit amet commodo nulla facilisi nullam vehicula ipsum. Scelerisque eleifend donec pretium vulputate sapien nec sagittis aliquam. Nunc mattis enim ut tellus elementum sagittis vitae et leo.&quot;</p>
           <p className="text-right">That One Guy - Wildlife Expert</p>
         </div>
       </section>
       </div>
-      <div className="px-32">
+      <div className="px-6 lg:px-32">
       <section id="opposition" className="text-center py-6">
         <h2 className="w-full inline-block mb-4 py-3 text-3xl text-center font-bold">The Opposition</h2>
         {/* attack the opposing organization here */}
-        <div className="text-2xl text-justify px-32">
+        <div className="md:text-2xl text-justify xl:px-32">
           <p className="py-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Etiam dignissim diam quis enim lobortis. Sed risus pretium quam vulputate dignissim suspendisse in est ante. </p>
           <p className="py-4">Fermentum leo vel orci porta non. Purus sit amet volutpat consequat mauris nunc congue nisi. Egestas tellus rutrum tellus pellentesque eu tincidunt. Purus in mollis nunc sed id semper risus in. Eget velit aliquet sagittis id consectetur purus ut. Egestas maecenas pharetra convallis posuere morbi leo. Sollicitudin nibh sit amet commodo nulla facilisi nullam vehicula ipsum.</p>
           <p className="py-4">Scelerisque eleifend donec pretium vulputate sapien nec sagittis aliquam. Nunc mattis enim ut tellus elementum sagittis vitae et leo.</p>
@@ -146,9 +146,9 @@ function IndexPage() {
       </div>
       <div className="w-full bg-gray-300 p-12">
       <section className="w-full inline block mb-4 text-3xl text-center font-bold">
-        <h2 className="p-4">Support Colorado&lsquo;s Ecology</h2>
-        <p className="p-4">Support Wolf Reintroduction</p>
-        <p className="p-4 text-6xl">Vote <span className="text-secondary border-4 border-secondary px-2">YES</span> on Prop 114</p>
+        <h2 className="py-4">Support Colorado&lsquo;s Ecology</h2>
+        <p className="py-4">Support Wolf Reintroduction</p>
+        <p className="py-4 text-6xl">Vote <span className="text-secondary border-4 border-secondary px-2">YES</span> on Proposition 114</p>
       </section>
       </div>
     </Layout>
