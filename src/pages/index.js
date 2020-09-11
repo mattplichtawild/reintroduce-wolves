57b1632c4f9b5c5e0910554cd422bd70ff110ac5
bigcatplichta <bigcatplichta@gmail.com>
2020-09-11 11:03:03 -0600

    adjust flex box

--- src/pages/index.js
+++ src/pages/index.js
@@ -71,46 +71,46 @@ function IndexPage() {
         </div>
       </section>
       </div>
-      <div className="px-32">
+      <div className="lg:px-32">
       <section id="facts" className="py-6">
         <div className="container flex flex-wrap mx-auto">
           <h2 className="w-full inline-block mb-4 text-3xl text-center font-bold">The Facts On Wolves</h2>
-          <div className="w-1/3 flex flex-grow flex-shrink p-6">
+          <div className="w-1/2 md:w-1/3 flex flex-grow flex-shrink p-2 md:p-6">
             <div className="flex-1 bg-white rounded-t rounded-b shadow-lg p-6">
               <h4 className="font-semibold pb-1 text-lg">First Fact</h4>
               <p className="font-light">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
             </div>
           </div>
 
-          <div className="w-1/3 flex flex-grow flex-shrink p-6">
+          <div className="w-1/2 md:w-1/3 flex flex-grow flex-shrink p-2 md:p-6">
             <div className="flex-1 bg-white rounded-t rounded-b shadow-lg p-6">
               <h4 className="font-semibold pb-1 text-lg">Second Fact</h4>
               <p className="font-light">Consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
             </div>
           </div>
 
-          <div className="w-1/3 flex flex-grow flex-shrink p-6">
+          <div className="w-1/2 md:w-1/3 flex flex-grow flex-shrink p-2 md:p-6">
             <div className="flex-1 bg-white rounded-t rounded-b shadow-lg p-6">
               <h4 className="font-semibold pb-1 text-lg">Third Fact</h4>
               <p className="font-light">Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
             </div>
           </div>
 
-          <div className="w-1/3 flex flex-grow flex-shrink p-6">
+          <div className="w-1/2 md:w-1/3 flex flex-grow flex-shrink p-2 md:p-6">
             <div className="flex-1 bg-white rounded-t rounded-b shadow-lg p-6">
               <h4 className="font-semibold pb-1 text-lg">Fourth Fact</h4>
               <p className="font-light">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
             </div>
           </div>
 
-          <div className="w-1/3 flex flex-grow flex-shrink p-6">
+          <div className="w-1/2 md:w-1/3 flex flex-grow flex-shrink p-2 md:p-6">
             <div className="flex-1 bg-white rounded-t rounded-b shadow-lg p-6">
               <h4 className="font-semibold pb-1 text-lg">Fifth Fact</h4>
               <p className="font-light"> Lorem ipsum dolor sit amet, consectetur adipiscing elit consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
             </div>
           </div>
 
-          <div className="w-1/3 flex flex-grow flex-shrink p-6">
+          <div className="w-1/2 md:w-1/3 flex flex-grow flex-shrink p-2 md:p-6">
             <div className="flex-1 bg-white rounded-t rounded-b shadow-lg p-6">
               <h4 className="font-semibold pb-1 text-lg">Sixth Fact</h4>
               <p className="font-light">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
